bce519a4fb4f76df32f70e700b62ac3d6a1eefc9
andygubser <andy.gubser@pm.me>
2020-05-28 20:45:08 +0200

    presentation fish

--- preprocessor/preprocessor.py
+++ preprocessor/preprocessor.py
@@ -5,13 +5,13 @@ class Preprocessor:
     """ input: raw dataframe of weather data,
     output: preprocessed dataframe with hourly datetimeindex """
 
-    def __init__(self, path_to_weather, local_time_column):
-        self.df_raw = pd.read_csv(path_to_weather)
+    def __init__(self, path_to_data, local_time_column, resampling_method):
+        self.df_raw = pd.read_csv(path_to_data)
         df_formatted_columns = self._format_columns(self.df_raw)
         df_formatted_datetime = self._format_datetime(df_formatted_columns,
                                                       local_time_column)
-        self.df_indexed_ch = self._set_datetime_index(df_formatted_datetime)
-        self.df_indexed_utc = self._set_datetime_index(df_formatted_datetime)
+        self.df_indexed_ch = self._set_datetime_index(df_formatted_datetime, resampling_method)
+        self.df_indexed_utc = self._set_datetime_index(df_formatted_datetime, resampling_method)
 
     @classmethod
     def _format_columns(cls, df):
@@ -34,7 +34,7 @@ class Preprocessor:
         return df
 
     @classmethod
-    def _set_datetime_index(cls, df, utc_time=True):
+    def _set_datetime_index(cls, df, resampling_method, utc_time=True):
         """ create datetime index based on local_time or utc,
         and resample mean per hour"""
         if utc_time:
@@ -43,4 +43,16 @@ class Preprocessor:
         else:
             df = (df.set_index(df["timestamp_ch"])
                     .drop(columns=["timestamp_utc"]))
-        return df.resample("h").mean().ffill()
+        return cls._resampling(df, resampling_method)
+
+    @classmethod
+    def _resampling(cls, df, resampling_method):
+        if resampling_method == "sum":
+            df_resampled = df.resample("h").sum().ffill()
+        elif resampling_method == "mean":
+            df_resampled = df.resample("h").mean().ffill()
+        df["hour"] = df.index.hour
+        df["week"] = df.index.week
+        df["month"] = df.index.month
+        return df_resampled
+
